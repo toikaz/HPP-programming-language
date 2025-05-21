deb2a4ea6b190a36fd3bafa50522ca879df8a2c4
toikaz <zxcursech@gmail.com>
2025-05-21 16:48:29 +0300

    examples

--- examples/Platformer.hpp
+++ examples/Platformer.hpp
@@ -1,53 +1,209 @@
 public static void Main(String[] args) {
     
-    func ot { System.out.print |0| | | ^ System.out.print | | | | ^ System.out.print | | | | }
-    func tt { System.out.print | |0| | ^ System.out.print | | | | ^ System.out.print | | | | }
     int x = 1
-    int y = 3
+    int y = 5
     str press = None
     System.out.print %x% %y%
     System.out.print %press%
-    System.out.print |0| | |
-    System.out.print | | | |
-    System.out.print | | | |
+    System.out.print |0| | | | |
+    System.out.print | | | | | |
+    System.out.print | | | | | |
+    System.out.print | | | | | |
+    System.out.print | | | | | |
     System.keyboard.read press
-    
-    if %press% == w : int y = %y% + 1
-    if %press% == s : int y = %y% - 1
-    if %press% == a : int x = %x% - 1
-    if %press% == d : int x = %x% + 1
-    if %press% == e : exit()
-    
+    if %x% == 6 : int %x% = %x% - 1
+    if %y% == 6 : int %y% = %y% - 1
+    if %x% == 0 : int %x% = %x% + 1
+    if %y% == 0 : int %y% = %y% + 1
+    if %press% == up : int y = %y% + 1
+    if %press% == down : int y = %y% - 1
+    if %press% == left : int x = %x% - 1
+    if %press% == right : int x = %x% + 1
+    if %press% == esc : exit()
+
     System.cls
-    
+
     System.out.print %x% %y%
     System.out.print %press%
     
-    if %x% == 1 : if %y% == 3 : call_fn ot
-    if %x% == 1 : if %y% == 2 : System.out.print | | | |
-    if %x% == 1 : if %y% == 2 : System.out.print |0| | |
-    if %x% == 1 : if %y% == 2 : System.out.print | | | |
-    if %x% == 1 : if %y% == 1 : System.out.print | | | |
-    if %x% == 1 : if %y% == 1 : System.out.print | | | |
-    if %x% == 1 : if %y% == 1 : System.out.print |0| | |
 
-    if %x% == 2 : if %y% == 3 : call_fn tt
-    if %x% == 2 : if %y% == 2 : System.out.print | | | |
-    if %x% == 2 : if %y% == 2 : System.out.print | |0| |
-    if %x% == 2 : if %y% == 2 : System.out.print | | | |
-    if %x% == 2 : if %y% == 1 : System.out.print | | | |
-    if %x% == 2 : if %y% == 1 : System.out.print | | | |
-    if %x% == 2 : if %y% == 1 : System.out.print | |0| |
+    
+    if %x% == 1 : if %y% == 5 : System.out.print |0| | | | |
+    if %x% == 1 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 5 : System.out.print | | | | | |
+    
+    if %x% == 1 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 4 : System.out.print |0| | | | |
+    if %x% == 1 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 4 : System.out.print | | | | | |
 
-    if %x% == 3 : if %y% == 3 : System.out.print | | |0|
-    if %x% == 3 : if %y% == 3 : System.out.print | | | |
-    if %x% == 3 : if %y% == 3 : System.out.print | | | |
-    if %x% == 3 : if %y% == 2 : System.out.print | | | |
-    if %x% == 3 : if %y% == 2 : System.out.print | | |0|
-    if %x% == 3 : if %y% == 2 : System.out.print | | | |
-    if %x% == 3 : if %y% == 1 : System.out.print | | | |
-    if %x% == 3 : if %y% == 1 : System.out.print | | | |
-    if %x% == 3 : if %y% == 1 : System.out.print | | |0|
+    if %x% == 1 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 3 : System.out.print |0| | | | |
+    if %x% == 1 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 3 : System.out.print | | | | | |
+   
+    if %x% == 1 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 2 : System.out.print |0| | | | |
+    if %x% == 1 : if %y% == 2 : System.out.print | | | | | |
+    
+    if %x% == 1 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 1 : if %y% == 1 : System.out.print |0| | | | |
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if %x% == 2 : if %y% == 5 : System.out.print | |0| | | |
+    if %x% == 2 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 5 : System.out.print | | | | | |
+    
+    if %x% == 2 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 4 : System.out.print | |0| | | |
+    if %x% == 2 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 4 : System.out.print | | | | | |
+
+    if %x% == 2 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 3 : System.out.print | |0| | | |
+    if %x% == 2 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 3 : System.out.print | | | | | |
+   
+    if %x% == 2 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 2 : System.out.print | |0| | | |
+    if %x% == 2 : if %y% == 2 : System.out.print | | | | | |
+    
+    if %x% == 2 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 2 : if %y% == 1 : System.out.print | |0| | | |
+
+
+
+
+
+
+
+
+
+
+    if %x% == 3 : if %y% == 5 : System.out.print | | |0| | |
+    if %x% == 3 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 5 : System.out.print | | | | | |
+    
+    if %x% == 3 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 4 : System.out.print | | |0| | |
+    if %x% == 3 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 4 : System.out.print | | | | | |
+
+    if %x% == 3 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 3 : System.out.print | | |0| | |
+    if %x% == 3 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 3 : System.out.print | | | | | |
+   
+    if %x% == 3 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 2 : System.out.print | | |0| | |
+    if %x% == 3 : if %y% == 2 : System.out.print | | | | | |
+    
+    if %x% == 3 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 3 : if %y% == 1 : System.out.print | | |0| | |
+
+
+
+    if %x% == 4 : if %y% == 5 : System.out.print | | | |0| |
+    if %x% == 4 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 5 : System.out.print | | | | | |
+    
+    if %x% == 4 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 4 : System.out.print | | | |0| |
+    if %x% == 4 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 4 : System.out.print | | | | | |
+
+    if %x% == 4 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 3 : System.out.print | | | |0| |
+    if %x% == 4 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 3 : System.out.print | | | | | |
+   
+    if %x% == 4 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 2 : System.out.print | | | |0| |
+    if %x% == 4 : if %y% == 2 : System.out.print | | | | | |
+    
+    if %x% == 4 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 4 : if %y% == 1 : System.out.print | | | |0| |
+
+
+
+
+
+
+
+    if %x% == 5 : if %y% == 5 : System.out.print | | | | |0|
+    if %x% == 5 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 5 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 5 : System.out.print | | | | | |
+    
+    if %x% == 5 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 4 : System.out.print | | | | |0|
+    if %x% == 5 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 4 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 4 : System.out.print | | | | | |
+
+    if %x% == 5 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 3 : System.out.print | | | | |0|
+    if %x% == 5 : if %y% == 3 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 3 : System.out.print | | | | | |
+   
+    if %x% == 5 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 2 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 2 : System.out.print | | | | |0|
+    if %x% == 5 : if %y% == 2 : System.out.print | | | | | |
+    
+    if %x% == 5 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 1 : System.out.print | | | | | |
+    if %x% == 5 : if %y% == 1 : System.out.print | | | | |0|
+
 
     goto 11
     
