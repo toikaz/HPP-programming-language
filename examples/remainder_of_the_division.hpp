6dd4776f81cc891f13913f0272b6fa9b494d6195
toikaz <zxcursech@gmail.com>
2025-05-23 23:56:38 +0300

    examples

---
+++ examples/remainder_of_the_division.hpp
@@ -0,0 +1,16 @@
+public static void Main(String[] args) {
+
+    System.out.print Enter first num:
+    int a = System.ReadInt()
+    System.out.print Enter second num:
+    int b = System.ReadInt()
+    int remains = %a% - 0
+    
+    int remains = %remains% - %b%
+    if %remains% > %b% : goto 6
+    if %remains% == %b% : goto 6
+
+    if %remains% < %b% : System.out.print the remainder of the division: %remains%
+    if %remains% < %b% : exit()
+
+}
