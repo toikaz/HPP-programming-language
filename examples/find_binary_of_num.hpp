757951cbd9448b6e5e2bd6676c331a924712b1a4
toikaz <zxcursech@gmail.com>
2025-05-24 07:45:19 +0300

    examples

---
+++ examples/find_binary_of_num.hpp
@@ -0,0 +1,29 @@
+public static void Main(String[] args) {
+    
+    System.out.print Enter num: (it takes a long time to calculate the answer)
+    
+    array output = []
+    
+    int a = System.ReadInt()
+    int b = 2
+    int remains = %a% - 0
+    
+    int remains = %remains% - %b%
+    if %remains% > %b% : goto 6
+    if %remains% == %b% : goto 6
+    int o = -1
+    int zero = 0
+    int g = -2
+    if %remains% == %o% : int remains = %remains% * %o%
+    if %remains% == %g% : int remain = %remains% + %b%
+    if %remains% != %g% : array add output %remains%
+    int cout = 999999999
+    int a = %a% / %b%
+    if %a% == %zero% : System.out.print Result:
+    if %a% == %zero% : System.out.print %cout%
+    if %a% == %zero% : array indexout output %cout%
+    if %a% == %zero% : int cout = %cout% + %o%
+    if %a% == %zero% : if %cout% != 0 : goto 18
+    if %cout% == %zero% : exit()
+    goto 4
+}
